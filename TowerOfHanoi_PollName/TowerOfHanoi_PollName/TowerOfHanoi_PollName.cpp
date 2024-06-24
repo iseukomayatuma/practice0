1ff68101a353ba660405c38846fa6f19207b82ec
松山煌晟 <kmm2459305@stu.o-hara.ac.jp>
2024-06-24 11:19:21 +0900

    演習5-7

---
+++ TowerOfHanoi_PollName/TowerOfHanoi_PollName/TowerOfHanoi_PollName.cpp
@@ -0,0 +1,42 @@
+//=========================================================
+//	ハノイの塔　解法
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>   // printf(),scanf()
+
+typedef enum {
+	A = 1,
+	B = 2,
+	C = 3,
+} POS;
+char ABCJudge[] = "ABC";
+
+// 関数プロトタイプ
+void move(int no, POS from, POS to);
+
+int main()
+{
+	while (true) {
+		int n;
+		do {
+			printf("ハノイの塔\a円盤の数:");
+			scanf_s("%d", &n);
+		} while (n < 1);
+
+		move(n, A, C);
+	}
+	return 0;
+}
+
+void move(int no, POS from, POS to)
+{
+	POS tmp = (POS)((A + B + C) - from - to);
+	if (no > 1) {
+		move(no - 1, from, tmp);
+	}
+	
+	printf("円盤[%d]を %c軸から %c軸へ移動\n", no, ABCJudge[from - 1], ABCJudge[to - 1]);
+	if (no > 1) {
+		move(no - 1, tmp, to);
+	}
+}
